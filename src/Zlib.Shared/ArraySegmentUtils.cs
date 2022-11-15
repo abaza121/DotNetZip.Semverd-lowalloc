7571da73746cf95dd576b5913c197f7861bcfd60
Mahmoud Abaza <abaza121@gmail.com>
2022-11-15 12:27:24 +0200

    Change Clear

--- src/Zlib.Shared/ArraySegmentUtils.cs
+++ src/Zlib.Shared/ArraySegmentUtils.cs
@@ -7,10 +7,6 @@ namespace Ionic.Zip.Utils
         public static void Copy<T>(ArraySegment<T> source, int sourceIndex, ArraySegment<T> destination, int destinationIndex, int length) => source.Slice(sourceIndex, length).CopyTo(destination.Slice(destinationIndex, length));
         public static void Copy<T>(ArraySegment<T> source, int sourceIndex, T[] destination, int destinationIndex, int length)             => source.Slice(sourceIndex, length).CopyTo(destination,destinationIndex);
         public static void Copy<T>(T[] source, int sourceIndex, ArraySegment<T> destination, int destinationIndex, int length)             => source.AsSpan(sourceIndex, length).CopyTo(destination.AsSpan(destinationIndex));
-
-        public static void Clear<T>(ArraySegment<T> target, int index, int length)
-        {
-            Array.Clear(target.Array, target.Offset + index, length);
-        }
+        public static void Clear<T>(ArraySegment<T> target, int index, int length)                                                         => target.AsSpan(index,length).Clear();
     }
 }
