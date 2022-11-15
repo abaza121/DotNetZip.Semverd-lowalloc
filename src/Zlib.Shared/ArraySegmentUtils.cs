bdeb7ce6ce605397b14ae38b1ccde2225ada7c70
Mahmoud Abaza <abaza121@gmail.com>
2022-11-15 12:22:25 +0200

    Slice and Span

--- src/Zlib.Shared/ArraySegmentUtils.cs
+++ src/Zlib.Shared/ArraySegmentUtils.cs
@@ -4,28 +4,12 @@ namespace Ionic.Zip.Utils
 {
     public static class ArraySegmentUtils
     {
-        public static void Copy<T>(ArraySegment<T> source, int sourceIndex, ArraySegment<T> destination, int destinationIndex, int length)
-        {
-            if (sourceIndex + length > source.Count || destinationIndex + length > destination.Count) throw new ArgumentOutOfRangeException();
-            source.Slice(sourceIndex, length).CopyTo(destination.Slice(destinationIndex, length));
-        }
-
-        public static void Copy<T>(ArraySegment<T> source, int sourceIndex, T[] destination, int destinationIndex, int length)
-        {
-            if (sourceIndex + length > source.Count) throw new ArgumentOutOfRangeException(nameof(source));
-            var destinationSegment = new ArraySegment<T>(destination, destinationIndex,length);
-            source.Slice(sourceIndex, length).CopyTo(destinationSegment);
-        }
-        public static void Copy<T>(T[] source, int sourceIndex, ArraySegment<T> destination, int destinationIndex, int length)
-        {
-            if (destinationIndex + length > destination.Count) throw new ArgumentOutOfRangeException(nameof(destination));
-            var sourceSegment = new ArraySegment<T>(source, sourceIndex, length);
-            sourceSegment.CopyTo(destination.Slice(destinationIndex, length));
-        }
+        public static void Copy<T>(ArraySegment<T> source, int sourceIndex, ArraySegment<T> destination, int destinationIndex, int length) => source.Slice(sourceIndex, length).CopyTo(destination.Slice(destinationIndex, length));
+        public static void Copy<T>(ArraySegment<T> source, int sourceIndex, T[] destination, int destinationIndex, int length)             => source.Slice(sourceIndex, length).CopyTo(destination,destinationIndex);
+        public static void Copy<T>(T[] source, int sourceIndex, ArraySegment<T> destination, int destinationIndex, int length)             => source.AsSpan(sourceIndex, length).CopyTo(destination.AsSpan(destinationIndex));
 
         public static void Clear<T>(ArraySegment<T> target, int index, int length)
         {
-            if (index + length > target.Count) throw new ArgumentOutOfRangeException(nameof(length));
             Array.Clear(target.Array, target.Offset + index, length);
         }
     }
